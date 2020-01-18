0473a516bdb80c847ce0340ee11fcb3ea02175e3
falcao-hawk <falcao_hawk@hotmail.com>
2020-01-18 00:18:39 -0300

    mapa mental das fases

---
+++ freemind/App-MyPetHealth - fase 1.mm
@@ -0,0 +1,111 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1579259039542" ID="ID_1708656346" MODIFIED="1579262069212">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      App MyPetHealth
+    </p>
+    <p style="text-align: center">
+      fase 1
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="gohome"/>
+<node CREATED="1579259646109" ID="ID_1331804861" MODIFIED="1579259707219" POSITION="right" TEXT="p&#xfa;blico alvo">
+<icon BUILTIN="group"/>
+<node CREATED="1579259660005" ID="ID_1858475938" MODIFIED="1579259680823" TEXT="propriet&#xe1;rios de animais de estima&#xe7;&#xe3;o">
+<node CREATED="1579259721693" ID="ID_707152317" MODIFIED="1579259725044" TEXT="c&#xe3;es"/>
+<node CREATED="1579259726013" ID="ID_1959344209" MODIFIED="1579259728373" TEXT="gatos"/>
+</node>
+</node>
+<node CREATED="1579259771088" ID="ID_1900165013" MODIFIED="1579262107538" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      cadastro - pet
+    </p>
+    <p>
+      (m&#250;ltiplos)
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1579261172663" ID="ID_954063236" MODIFIED="1579263814805" TEXT="nome"/>
+<node CREATED="1579261176453" ID="ID_1507797250" MODIFIED="1579263819995" TEXT="apelido"/>
+<node CREATED="1579259799848" ID="ID_368458438" MODIFIED="1579259805396" TEXT="data de nascimento"/>
+<node CREATED="1579259806095" ID="ID_459516935" MODIFIED="1579259817938" TEXT="peso"/>
+<node CREATED="1579259818776" ID="ID_930460696" MODIFIED="1579259827566" TEXT="dimens&#xf5;es">
+<node CREATED="1579261364364" ID="ID_46808907" MODIFIED="1579261368060" TEXT="porte">
+<node CREATED="1579261376477" ID="ID_222643386" MODIFIED="1579261383261" TEXT="micro"/>
+<node CREATED="1579261384085" ID="ID_1436323568" MODIFIED="1579261386449" TEXT="pequeno"/>
+<node CREATED="1579261388688" ID="ID_743679909" MODIFIED="1579261391889" TEXT="m&#xe9;dio"/>
+<node CREATED="1579261392950" ID="ID_557105586" MODIFIED="1579261394889" TEXT="grande"/>
+</node>
+</node>
+<node CREATED="1579264073715" ID="ID_26745159" MODIFIED="1579264097986" TEXT="esp&#xe9;cie">
+<node CREATED="1579264084750" ID="ID_1795292200" MODIFIED="1579264090122" TEXT="c&#xe3;o"/>
+<node CREATED="1579264090930" ID="ID_320156477" MODIFIED="1579264093254" TEXT="gato"/>
+</node>
+<node CREATED="1579259828222" ID="ID_6226172" MODIFIED="1579259830737" TEXT="ra&#xe7;a"/>
+<node CREATED="1579259834357" ID="ID_267867318" MODIFIED="1579259850556" TEXT="pedigree"/>
+<node CREATED="1579259852205" ID="ID_1456280668" MODIFIED="1579261167577" TEXT="RG-CHIP"/>
+</node>
+<node CREATED="1579261200203" ID="ID_1313368131" MODIFIED="1579263796346" POSITION="right" TEXT="cadastro - dono">
+<icon BUILTIN="male2"/>
+<node CREATED="1579261236265" ID="ID_1303230134" MODIFIED="1579261238317" TEXT="nome"/>
+<node CREATED="1579261239231" ID="ID_415973199" MODIFIED="1579261242908" TEXT="endere&#xe7;o"/>
+<node CREATED="1579261243541" ID="ID_221001767" MODIFIED="1579261247365" TEXT="telefones"/>
+<node CREATED="1579268149017" ID="ID_1339993000" MODIFIED="1579268153479" TEXT="e-mail"/>
+</node>
+<node CREATED="1579259733942" ID="ID_331274702" MODIFIED="1579261484263" POSITION="left" TEXT="objetivos">
+<node CREATED="1579261502710" ID="ID_991542044" MODIFIED="1579261518815" TEXT="cadastro - vacinas"/>
+<node CREATED="1579261730450" ID="ID_1817747520" MODIFIED="1579262150196" TEXT="alertas">
+<node CREATED="1579261743301" ID="ID_1177121166" MODIFIED="1579261753538" TEXT="vencimento vacina&#xe7;&#xe3;o"/>
+<node CREATED="1579261754889" ID="ID_1595933125" MODIFIED="1579261761555" TEXT="aus&#xea;ncia de vacina&#xe7;&#xe3;o"/>
+</node>
+</node>
+<node CREATED="1579261587534" ID="ID_1725736804" MODIFIED="1579261714168" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      base dados
+    </p>
+    <p style="text-align: center">
+      auxiliar
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1579261608781" ID="ID_1704197637" MODIFIED="1579261619382" TEXT="nomes vacinas">
+<node CREATED="1579261620350" ID="ID_1659625124" MODIFIED="1579261627912" TEXT="prazos vacinas"/>
+</node>
+<node CREATED="1579261902150" ID="ID_918871657" MODIFIED="1579261910711" TEXT="nomes verm&#xed;fugos">
+<node CREATED="1579261911906" ID="ID_1670825763" MODIFIED="1579261919515" TEXT="prazos verm&#xed;fugos"/>
+</node>
+<node CREATED="1579261831475" ID="ID_1379263990" MODIFIED="1579261861528">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      veterin&#225;rios
+    </p>
+    <p>
+      ou cl&#237;nicas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</map>
